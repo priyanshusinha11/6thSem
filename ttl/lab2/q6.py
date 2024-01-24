1c15dba75fb1055459a75714e049e0756550d40c
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-01-24 09:27:23 +0530

    ttl lab2

--- ttl/lab2/q6.py
+++ ttl/lab2/q6.py
@@ -1,2 +1,21 @@
 #armstrong
 
+print("Enter the number")
+num = int(input())
+temp = num
+cnt = 0
+
+while temp != 0:
+    temp = temp // 10
+    cnt += 1
+
+temp = num
+sum = 0
+while temp != 0:
+    sum += (temp % 10) ** cnt
+    temp = temp // 10
+
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
