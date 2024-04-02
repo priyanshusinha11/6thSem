d1824cb93b9e9bbb50af7a7d9d2186481f1068be
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-04-02 17:03:01 +0530

    test

---
+++ test/Q1.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Process {
+    int id;
+    int at;
+    int bt;
+
+    Process(int id, int at, int bt) {
+        this.id = id;
+        this.at = at;
+        this.bt = bt;
+    }
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Process> prc = new ArrayList<>();
+        System.out.println("Enter the number of processes:");
+        int n = sc.nextInt();
+        System.out.println("Enter the arrival and burst time:");
+        for (int i = 0; i < n; i++) {
+            int at = sc.nextInt();
+            int bt = sc.nextInt();
+            prc.add(new Process(i, at, bt));
+        }
+
+        prc.sort(Comparator.comparingInt(p -> p.bt));
+
+        int ct = 0;
+        int totalTat = 0;
+        int totalWt = 0;
+
+        for (Process process : prc) {
+            if (process.at > ct) {
+                ct = process.at;
+            }
+
+            ct += process.bt;
+
+            int tat = ct - process.at;
+            int wt = tat - process.bt;
+            totalTat += tat;
+            totalWt += wt;
+
+            System.out.println("Process " + process.id +
+                    " | Turnaround Time: " + tat +
+                    " | Waiting Time: " + wt);
+        }
+
+        double avgTat = (double) totalTat / prc.size();
+        double avgWt = (double) totalWt / prc.size();
+
+        System.out.println("\nAverage Turnaround Time: " + avgTat);
+        System.out.println("Average Waiting Time: " + avgWt);
+
+        sc.close();
+    }
+}
---
+++ test/Q2.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Process {
+    int id;
+    int at;
+    int bt;
+    int rt;
+
+    Process(int id, int at, int bt) {
+        this.id = id;
+        this.at = at;
+        this.bt = bt;
+        this.rt = bt;
+    }
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Process> arrivalQueue = new LinkedList<>();
+        System.out.println("Enter the number of processes:");
+        int n = sc.nextInt();
+        System.out.println("Enter the arrival and burst time:");
+        for (int i = 0; i < n; i++) {
+            int at = sc.nextInt();
+            int bt = sc.nextInt();
+            arrivalQueue.add(new Process(i, at, bt));
+        }
+
+        PriorityQueue<Process> processQueue = new PriorityQueue<>(Comparator.comparingInt(p -> p.rt));
+        int currentTime = 0;
+        int completed = 0;
+        int totalTat = 0;
+        int totalWt = 0;
+
+        while (completed < n) {
+            while (!arrivalQueue.isEmpty() && arrivalQueue.peek().at <= currentTime) {
+                processQueue.add(arrivalQueue.poll());
+            }
+
+            if (processQueue.isEmpty()) {
+                currentTime++;
+                continue;
+            }
+
+            Process currentProcess = processQueue.poll();
+            int executionTime = Math.min(currentProcess.rt, 1);
+            currentProcess.rt -= executionTime;
+            currentTime += executionTime;
+
+            if (currentProcess.rt == 0) {
+                completed++;
+                int tat = currentTime - currentProcess.at;
+                int wt = tat - currentProcess.bt;
+                totalTat += tat;
+                totalWt += wt;
+                System.out.println(
+                        "Process " + currentProcess.id + " | Turnaround Time: " + tat + " | Waiting Time: " + wt);
+            } else {
+                processQueue.add(currentProcess);
+            }
+        }
+
+        double avgTat = (double) totalTat / n;
+        double avgWt = (double) totalWt / n;
+
+        System.out.println("\nAverage Turnaround Time: " + avgTat);
+        System.out.println("Average Waiting Time: " + avgWt);
+
+        sc.close();
+    }
+}
